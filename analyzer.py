cb3d9c1e75faa7a09ba9091030ce0e797ea345ac
Moshe Silber <mosheyz770@gmail.com>
2026-02-10 12:25:59 +0200

    add check_sensitive_port function

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,9 @@
+from config import sensitive_ports
+
 def check_outside_ip(file):
     outside = [line[1] for line in file if not line[1].startswith("192.168") and not line[1].startswith("10.")]
     return outside
+
+def check_sensitive_port(file):
+    sensitive = [line for line in file if line[3] in sensitive_ports]
+    return sensitive
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from reader import load_csv
-from analyzer import check_outside_ip
-
+from analyzer import check_outside_ip, check_sensitive_port
 
 load_csv("network_traffic.log")
 check_outside_ip(load_csv("network_traffic.log"))
+check_sensitive_port(load_csv("network_traffic.log"))
